9ab93283626b64b200505833f24d8efd2ee2342b
Clay1612 <dedsec1612@gmail.com>
2021-06-06 13:57:55 +0300

    финальные правки

--- app/js/index.js
+++ app/js/index.js
@@ -62,7 +62,7 @@ sendPostRequest('POST', requestURL, body)
 
 //Template functions
 function createCard() {
-  return cardTemplate;
+  return cardTemplate;     //Эта функция особо то и не нужна, не до конца видимо понял твою задумку, обсудим.
 }
 
 function addElement() {
