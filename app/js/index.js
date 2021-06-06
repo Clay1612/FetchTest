b185c27d55e5c9f2b083a593a1d703ecba058b62
Clay1612 <dedsec1612@gmail.com>
2021-06-06 13:45:12 +0300

    Template done

--- app/js/index.js
+++ app/js/index.js
@@ -1,17 +1,17 @@
 'use strict'
 
-//Variables
+//Request URL
 const requestURL = 'https://jsonplaceholder.typicode.com/comments';
-
-let titleContent = document.querySelectorAll('.card-title');
-let subTutleContent = document.querySelectorAll('.card-subtitle');
-let cardContent = document.querySelectorAll('.card-text');
 
 let body = {
   title: 'Lorem ipsum dolor sit amet',
   subtitle: 'qwertyui@gmail.com',
   text: 'consectetur adipisicing elit',
 }
+
+//Template variables
+let cardTemplate = document.querySelector('.card-template');
+let layoutRow = document.querySelector('.row');
 
 // RequestFunction
 //GET
@@ -43,18 +43,43 @@ function sendPostRequest(method, url, body = null) {
 sendGetRequest(requestURL)
   .then(data => {
     for(let i of titleContent) {
-      i.innerHTML = data[0].name;
+      i.innerHTML = data[0].name;  //** FIX ME */
     }
 
     for(let i of subTutleContent) {
-      i.innerHTML = data[0].email;
+      i.innerHTML = data[0].email; //** FIX ME */
     }
 
     for(let i of cardContent) {
-      i.innerHTML = data[0].body;
+      i.innerHTML = data[0].body; //** FIX ME */
     }
   })
 
 //POST
 sendPostRequest('POST', requestURL, body)
   .then(data => console.log(data));
+
+
+//Template functions
+function createCard() {
+  return cardTemplate;
+}
+
+function addElement() {
+  let fragment = new DocumentFragment();
+  fragment.append(createCard().content.cloneNode(true));
+  layoutRow.append(fragment);
+}
+
+
+//Template add
+addElement();
+addElement();
+addElement();
+addElement();
+addElement();
+addElement();
+
+let titleContent = document.querySelectorAll('.card-title');
+let subTutleContent = document.querySelectorAll('.card-subtitle');
+let cardContent = document.querySelectorAll('.card-text');
--- dist/js/index.js
+++ dist/js/index.js
@@ -1,4 +1,4 @@
-'use strict'; //Variables
+'use strict'; //Request URL
 
 function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
 
@@ -7,14 +7,14 @@ function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o =
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
 var requestURL = 'https://jsonplaceholder.typicode.com/comments';
-var titleContent = document.querySelectorAll('.card-title');
-var subTutleContent = document.querySelectorAll('.card-subtitle');
-var cardContent = document.querySelectorAll('.card-text');
 var body = {
   title: 'Lorem ipsum dolor sit amet',
   subtitle: 'qwertyui@gmail.com',
   text: 'consectetur adipisicing elit'
-}; // RequestFunction
+}; //Template variables
+
+var cardTemplate = document.querySelector('.card-template');
+var layoutRow = document.querySelector('.row'); // RequestFunction
 //GET
 
 function sendGetRequest(url) {
@@ -47,7 +47,7 @@ sendGetRequest(requestURL).then(function (data) {
   try {
     for (_iterator.s(); !(_step = _iterator.n()).done;) {
       var i = _step.value;
-      i.innerHTML = data[0].name;
+      i.innerHTML = data[0].name; //** FIX ME */
     }
   } catch (err) {
     _iterator.e(err);
@@ -61,7 +61,7 @@ sendGetRequest(requestURL).then(function (data) {
   try {
     for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
       var _i = _step2.value;
-      _i.innerHTML = data[0].email;
+      _i.innerHTML = data[0].email; //** FIX ME */
     }
   } catch (err) {
     _iterator2.e(err);
@@ -75,7 +75,7 @@ sendGetRequest(requestURL).then(function (data) {
   try {
     for (_iterator3.s(); !(_step3 = _iterator3.n()).done;) {
       var _i2 = _step3.value;
-      _i2.innerHTML = data[0].body;
+      _i2.innerHTML = data[0].body; //** FIX ME */
     }
   } catch (err) {
     _iterator3.e(err);
@@ -86,5 +86,26 @@ sendGetRequest(requestURL).then(function (data) {
 
 sendPostRequest('POST', requestURL, body).then(function (data) {
   return console.log(data);
-});
+}); //Template functions
+
+function createCard() {
+  return cardTemplate;
+}
+
+function addElement() {
+  var fragment = new DocumentFragment();
+  fragment.append(createCard().content.cloneNode(true));
+  layoutRow.append(fragment);
+} //Template add
+
+
+addElement();
+addElement();
+addElement();
+addElement();
+addElement();
+addElement();
+var titleContent = document.querySelectorAll('.card-title');
+var subTutleContent = document.querySelectorAll('.card-subtitle');
+var cardContent = document.querySelectorAll('.card-text');
 //# sourceMappingURL=index.js.map
